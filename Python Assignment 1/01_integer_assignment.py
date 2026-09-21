194fbacc1b5232e151e61c876b6d2dc514e47bb7
Govardhan V_DS <vasappagarigovardhangowd@gmail.com>
2026-09-21 14:17:50 +0530

    Update 01_integer_assignment.py

--- Python Assignment 1/01_integer_assignment.py
+++ Python Assignment 1/01_integer_assignment.py
@@ -17,40 +17,79 @@ print("-" * 50)
 
 # Question 1: Calculate the product of first 10 natural numbers
 print("Question 1: Calculate the product of first 10 natural numbers")
-# Your code here
+
+product=1
+for i in range(1,11):
+  product=product*i
+print(product)
+
 
 # Question 2: Find the remainder when 156 is divided by 7
 print("\nQuestion 2: Find the remainder when 156 is divided by 7")
-# Your code here
+
+a=157
+b=7
+print(a%b)
+
+
 
 # Question 3: Calculate the square of 25
 print("\nQuestion 3: Calculate the square of 25")
-# Your code here
+
+a=25
+print(a*a)
+
 
 # Question 4: Find the cube root of 125
 print("\nQuestion 4: Find the cube root of 125")
-# Your code here
+
+a=125
+print(round(a**(1/3)))
+
 
 # Question 5: Calculate the sum of digits in number 12345
 print("\nQuestion 5: Calculate the sum of digits in number 12345")
-# Your code here
+
+a=12345
+total=sum(int(i) for i in str(a))
 
 # Question 6: Check if 97 is a prime number
 print("\nQuestion 6: Check if 97 is a prime number")
-# Your code here
+
+n=97
+for i in range(2,n):
+  if n%i==0:
+    print("Not a Prime a number")
+  else:
+    print("Prime number")
 
 # Question 7: Find the factorial of 8
 print("\nQuestion 7: Find the factorial of 8")
-# Your code here
+
+n=1
+for i in range(1,9):
+  n=n*i
+print(n)
+
+
 
 # Question 8: Calculate the average of numbers: 15, 23, 31, 42, 56
 print("\nQuestion 8: Calculate the average of numbers: 15, 23, 31, 42, 56")
-# Your code here
+
+a=["15,23,31,42,56"]
+avg=sum(a)/len(a)
+
 
 # Question 9: Find the greatest common divisor (GCD) of 48 and 36
 print("\nQuestion 9: Find the greatest common divisor (GCD) of 48 and 36")
-# Your code here
+
+import math
+print(math.gcd(48,36))
 
 # Question 10: Calculate the sum of first 20 odd numbers
 print("\nQuestion 10: Calculate the sum of first 20 odd numbers")
-# Your code here 
+
+total=0
+for i in range(1,40,2):
+  total=total+i
+print(total)
